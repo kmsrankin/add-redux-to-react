0277044fa6a8f912c792b246c9e65160015a20bf
Keegan Rankin <Kms.rankin@gmail.com>
2019-06-27 17:43:40 -0400

    added redux to GroceryPageContainer

--- src/modules/groceries.js
+++ src/modules/groceries.js
@@ -3,13 +3,26 @@ const initialState = {
   name: ''
 }
 
+const ADD_GROCERY = 'ADD_GROCERY'
+
+const addNewGrocery = grocery => {
+  return {
+    type: ADD_GROCERY,
+    grocery
+  }
+}
+
 const groceries = (state = initialState, action) => {
   switch(action.type) {
+    case ADD_GROCERY:
+      const newGroceries = state.groceryList.concat(action.grocery)
+      return {...state, groceryList: newGroceries }
     default:
       return state
   }
 }
 
 export {
+  addNewGrocery,
   groceries
 }

91f727ab9d11dc394a64c79730a58fcccebef950
Keegan Rankin <Kms.rankin@gmail.com>
2019-06-27 18:08:40 -0400

    added redux to GroceryFormContainer

--- src/modules/groceries.js
+++ src/modules/groceries.js
@@ -3,6 +3,20 @@ const initialState = {
   name: ''
 }
 
+const groceries = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_GROCERY:
+      const newGroceries = state.groceryList.concat(action.grocery)
+      return {...state, groceryList: newGroceries }
+    case HANDLE_NAME_CHANGE:
+      return {...state, name: action.newName}
+    case CLEAR_FORM:
+      return {...state, name: ''}
+    default:
+      return state
+  }
+}
+
 const ADD_GROCERY = 'ADD_GROCERY'
 
 const addNewGrocery = grocery => {
@@ -12,17 +26,27 @@ const addNewGrocery = grocery => {
   }
 }
 
-const groceries = (state = initialState, action) => {
-  switch(action.type) {
-    case ADD_GROCERY:
-      const newGroceries = state.groceryList.concat(action.grocery)
-      return {...state, groceryList: newGroceries }
-    default:
-      return state
+const HANDLE_NAME_CHANGE = 'HANDLE_NAME_CHANGE'
+
+const handleNameChange = event => {
+  const newName = event.target.value
+  return {
+    type: HANDLE_NAME_CHANGE,
+    newName
+  }
+}
+
+const CLEAR_FORM = 'CLEAR_FORM'
+
+const clearForm = () => {
+  return {
+    type: CLEAR_FORM
   }
 }
 
 export {
   addNewGrocery,
-  groceries
+  clearForm,
+  groceries,
+  handleNameChange
 }
